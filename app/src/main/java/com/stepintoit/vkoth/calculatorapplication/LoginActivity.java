7ff393b0468d6628d0f6133e3659e65cbb6842da
vkoth <vkothiwala6@gmail.com>
2018-11-05 22:33:45 -0500

    with email validation

--- app/src/main/java/com/stepintoit/vkoth/calculatorapplication/LoginActivity.java
+++ app/src/main/java/com/stepintoit/vkoth/calculatorapplication/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -50,13 +51,17 @@ public class LoginActivity extends AppCompatActivity {
                 } else if (pass.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Password can not be empty", Toast.LENGTH_SHORT).show();
 
-                } else {
+                } else if (Patterns.EMAIL_ADDRESS.matcher(uname).matches()){
 
                     MySharedPreference.getInstance(LoginActivity.this).putValue(MySharedPreference.KEY_USER, uname);
                     MySharedPreference.getInstance(LoginActivity.this).putValue(MySharedPreference.KEY_PASSWORD,pass);
                     startActivity(new Intent(LoginActivity.this, CalculatorActivity.class));
                     finish();
 
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Invalid email id",Toast.LENGTH_LONG).show();
                 }
             }
         });
